2462587a280bc77ea8c1a34f1a5e91f6ba2ce0d4
Promode <thetestingacademy@gmail.com>
2024-05-24 12:47:43 +0530

    Day 2 - problem 1

---
+++ Day2/Lab011.py
@@ -0,0 +1,18 @@
+# Input - 2,
+# 2*1 = 2
+# 2*2 = 4
+# 2*10 = 20
+
+# 1. - Input Users
+# 2. - Rough Logic
+# 3. -  Test Code
+
+user_input   = int(input("Enter your Number!"))
+print("Printing the Table")
+
+print(f"{user_input}*1={user_input*1}")
+print(f"{user_input}*2={user_input*2}")
+print(f"{user_input}*3={user_input*3}")
+print(f"{user_input}*4={user_input*4}")
+print(f"{user_input}*5={user_input*5}")
+print(f"{user_input}*10={user_input*10}")
