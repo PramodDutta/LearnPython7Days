193c95c45fd0b898779de706bffcf57f30f132c0
Promode <thetestingacademy@gmail.com>
2024-05-24 12:43:08 +0530

    Day 2

---
+++ Day2/Lab008.py
@@ -0,0 +1,12 @@
+age = 65
+age = age+1
+print(age)
+
+# int -> -1,-5 0, 12, 10000 9999
+# float - 3.14, 24.56
+# variableName = variableValue
+
+
+class = 45
+print(age)
+
---
+++ Day2/Lab010.py
@@ -0,0 +1,4 @@
+_123 = 65
+print(_123)
+_pramod = 34
+print(_pramod)
